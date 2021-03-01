54bccfed36adc583c530b8536b633774b6528732
Ardityo Mulawarman <ardityo.mulawarman@tokopedia.com>
2021-03-01 11:18:16 +0700

    init config

---
+++ vars/notify.groovy
@@ -0,0 +1,11 @@
+import com.corenux.Constants
+
+def call(Map config=[:]) {
+    if (config.type == "slack") {
+        echo Constants.SLACK_MESSAGE
+        echo config.message
+    } else {
+        echo Constants.EMAIL_MESSAGE
+        echo config.message
+    }
+}
